991baa20a58b773fa9aac62659ad2d7e140f5adb
rogeriocassares <roger10cassares@gmail.com>
2025-11-27 00:50:53 -0300

    add web, mobile, server and telemetry as a monorepo

---
+++ packages/api/src/eden-server.ts
@@ -0,0 +1,11 @@
+import { edenTreaty } from "@elysiajs/eden";
+import type { App } from "server";
+
+if (!process.env.SERVER_API) {
+  throw new Error("SERVER_API must be set");
+}
+
+export const api: ReturnType<typeof edenTreaty<App>> = edenTreaty<App>(
+  process.env.SERVER_API,
+  { $fetch: { credentials: "include" } }
+);
